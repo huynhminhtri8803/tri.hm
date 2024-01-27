64b73cc2371e50805f1d6941f1f734ec8a727715
huynhminhtri8803 <157440859+huynhminhtri8803@users.noreply.github.com>
2024-01-27 07:31:05 +0700

    Add files via upload

---
+++ web-trangphuc/js/costum.js
@@ -0,0 +1,78 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+	
+	  })
+
+$('#product-sale').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+$('#product-giaytennis').owlCarousel({
+   	loop:true,	// cho lap lại		
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   	 autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+	margin:40,
+// xuất hiện item tùy theo thiết bị (pc/ ipad/phone)i
+
+
+	 responsive:{
+        0:{
+            items:1,
+           dots:false,
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+	
+	  })
+
+
+
